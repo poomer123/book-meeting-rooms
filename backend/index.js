1167c4ca963199fb8150c9584f132b880280170c
ApiratChiwakanit <apirat.c@roundtwosolutions.com>
2018-12-03 11:28:34 +0700

    install dependencies , config and test for backend

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,44 @@
 const express = require('express')
 const server = express()
+const session = require('express-session')
+const bodyParser = require('body-parser')
+const { check, validationResult } = require('express-validator/check')
 const PORT = 3000
 
+// session config
+server.use(session({
+    secret: 'kickdown.in.th',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {}
+}))
+
+// bodyParser config
+server.use(bodyParser.urlencoded({ extended: false }))
+server.use(bodyParser.json())
+
+server.post('/', 
+    [
+        check('name').not().isEmpty()
+    ], 
+    (req, res) => {
+        try {
+            validationResult(req).throw()
+            res.json(req.body)
+        } catch (error) {
+            res.status(400).json({message: error.message})
+        }
+    }
+)
+
+server.get('/session', (req, res) => {
+    req.session.item = 'hello world'
+    res.end('set session')
+})
+
+
 server.get('*', (req, res) => {
-    res.end('<h1>Backend Server is Started</h1>')
+    res.end(`<h1>Backend Server is Started. Session is ${req.session.item}</h1>`)
 })
 
 server.listen(PORT, () => console.log('Server is started port : '+ PORT))
