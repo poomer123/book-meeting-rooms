5f27fc2cb1705ccda99c526a28b822c770c205ce
ApiratChiwakanit <apirat.c@roundtwosolutions.com>
2018-12-03 11:50:18 +0700

    clean up the file, install bootstrap, juery and config it

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,8 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min'
+import * as jquery from 'jquery'
 
 Vue.config.productionTip = false
+Vue.prototype.jquery = jquery
 
 new Vue({
   router,
